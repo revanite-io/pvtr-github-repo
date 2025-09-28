bb28c33703d10ac8ac854d030d57ce225fd3d5a8
Travis Truman <trumant@gmail.com>
2025-09-28 14:08:40 -0400

    feat: improve AC-04.01 evalaution coverage
    This change adds logic to check the value of `can_approve_pull_request_reviews`
    as the creation and approval of PRs is arguably a write permission grant.
    
    See https://docs.github.com/en/rest/actions/permissions?apiVersion=2022-11-28#get-default-workflow-permissions-for-a-repository and https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/enabling-features-for-your-repository/managing-github-actions-settings-for-a-repository#preventing-github-actions-from-creating-or-approving-pull-requests for reference to behavior
    of the GitHub configuration and REST API.
    
    Signed-off-by: Travis Truman <trumant@gmail.com>

--- evaluation_plans/osps/access_control/steps_test.go
+++ evaluation_plans/osps/access_control/steps_test.go
@@ -67,3 +67,74 @@ func Test_orgRequiresMFA(t *testing.T) {
 		})
 	}
 }
+
+// See https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/enabling-features-for-your-repository/managing-github-actions-settings-for-a-repository#setting-the-permissions-of-the-github_token-for-your-repository
+func Test_workflowDefaultReadPermissions(t *testing.T) {
+	tests := []struct {
+		name        string
+		payload     data.Payload
+		wantResult  layer4.Result
+		wantMessage string
+	}{
+		{
+			name: "read permissions and no PR permissions",
+			payload: data.Payload{
+				RestData: &data.RestData{
+					WorkflowPermissions: data.WorkflowPermissions{
+						DefaultPermissions:    "read", // read access for the contents and packages permissions
+						CanApprovePullRequest: false,  // cannot create or approve PRs
+					},
+				},
+			},
+			wantResult:  layer4.Passed,
+			wantMessage: "Workflow permissions default to read only.",
+		},
+		{
+			name: "read permissions, but allows PR approvals",
+			payload: data.Payload{
+				RestData: &data.RestData{
+					WorkflowPermissions: data.WorkflowPermissions{
+						DefaultPermissions:    "read", // read access for the contents and packages permissions
+						CanApprovePullRequest: true,   // can create & approve PRs
+					},
+				},
+			},
+			wantResult:  layer4.Failed,
+			wantMessage: "Workflow permissions default to read only for contents and packages, but PR approval is permitted.",
+		},
+		{
+			name: "write permissions and no PR permissions",
+			payload: data.Payload{
+				RestData: &data.RestData{
+					WorkflowPermissions: data.WorkflowPermissions{
+						DefaultPermissions:    "write", // read & write access for all permission scopes
+						CanApprovePullRequest: false,   // cannot create or approve PRs (in theory at least)
+					},
+				},
+			},
+			wantResult:  layer4.Failed,
+			wantMessage: "Workflow permissions default to read/write, but PR approval is forbidden.",
+		},
+		{
+			name: "write permissions and PR permissions",
+			payload: data.Payload{
+				RestData: &data.RestData{
+					WorkflowPermissions: data.WorkflowPermissions{
+						DefaultPermissions:    "write",
+						CanApprovePullRequest: true,
+					},
+				},
+			},
+			wantResult:  layer4.Failed,
+			wantMessage: "Workflow permissions default to read/write and PR approval is permitted.",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotResult, gotMessage := workflowDefaultReadPermissions(tt.payload, map[string]*layer4.Change{})
+			assert.Equal(t, tt.wantResult, gotResult)
+			assert.Equal(t, tt.wantMessage, gotMessage)
+		})
+	}
+}
