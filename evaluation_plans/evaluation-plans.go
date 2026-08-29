540c3e7e114a302c70f77ac7c5a29660af4820cc
Eddie Knight <knight@linux.com>
2026-08-29 10:21:02 -0500

    Added  reusable step to support OSPS-BR-01.02
    Signed-off-by: Eddie Knight <knight@linux.com>

--- evaluation_plans/evaluation-plans.go
+++ evaluation_plans/evaluation-plans.go
@@ -40,10 +40,9 @@ var (
 			build_release.CicdSanitizedInputParameters,
 		},
 		"OSPS-BR-01.02": {
-			// Retired in v2026.08.28: coverage was split
+			// Retired in v2026.10.25: coverage was split
 			// between BR-01.01 and BR-01.03
-			build_release.CicdSanitizedInputParameters,
-			build_release.CicdUntrustedCodeIsolation,
+			reusable_steps.Retired,
 		},
 		"OSPS-BR-01.03": {
 			build_release.CicdUntrustedCodeIsolation,
